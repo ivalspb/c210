06ede761056ad9f0f58bfb78d93a6e40295ead09
ivalspb <ival.spb@gmail.com>
2023-12-04 09:14:54 +0300

    lab2 cout node--

--- lab2-c210/MyStack2.h
+++ lab2-c210/MyStack2.h
@@ -12,20 +12,17 @@ class MyStack2
 		T data;
 		Node2* pNext;
 		friend class MyStack2<T>;
-		/*template<typename T> friend ostream& operator<<(ostream& stream, const Node2& node_to_print)
-		{
-			return stream << node_to_print.p_data << " ";
-		};*/
+		template<typename T>
+		friend ostream& operator<<(ostream& out, const MyStack2<T>& stack_to_print);
+		template<typename T> 
+		friend ostream& operator<<(ostream& stream,  const typename MyStack2<T>::Node2& node_to_print);
+		
 	public:
 		Node2();
 		~Node2();
-		//Node2(Node2& other_node);
-		//Node(Node&& tmp_node);
-		//Node2(const T& data_src);
-		//Node2(T&& data_tmp);
-		//Node& operator=(const Node& other);
-		//Node& operator=(Node&& tmp);
-	};
+		Node2(const T& data_src);
+		Node2(T&& data_tmp);
+		};
 	size_t m_size;
 	Node2 Head;
 
@@ -39,10 +36,13 @@ public:
 	
 	void push(const T& elem_to_push);
 	void push(T&& tmp_e);
-	T& pop();
+	T pop();
 
-	template<typename U>
-	friend ostream& operator<<(ostream& out, const MyStack2<U>& stack_to_print);
+	template<typename T>
+	friend ostream& operator<<(ostream& stream, const typename MyStack2<T>::Node2& node_to_print);
+
+	template<typename T>
+	friend ostream& operator<<(ostream& out, const MyStack2<T>& stack_to_print);
 };
 
 
@@ -55,15 +55,15 @@ inline MyStack2<T>::Node2::~Node2()
 	/*if (this) this = this->pNext;
 	else throw std::out_of_range("try to del empty node!");*/
 }
-//
-//template<typename T>
-//inline MyStack2<T>::Node2::Node2(const T& data_src):data(data_src){}
-//
-//template<typename T>
-//inline MyStack2<T>::Node2::Node2(T&& data_tmp)
-//{
-//	data = std::move(data_tmp);
-//}
+
+template<typename T>
+inline MyStack2<T>::Node2::Node2(const T& data_src):data(data_src){}
+
+template<typename T>
+inline MyStack2<T>::Node2::Node2(T&& data_tmp)
+{
+	data = std::move(data_tmp);
+}
 
 template<typename T>
 inline MyStack2<T>::MyStack2()
@@ -79,11 +79,15 @@ inline MyStack2<T>::MyStack2(const MyStack2<T>& other_stack_list)//в голове данн
 	if (other_stack_list.m_size)//если источник не пустой
 	{
 		Node2* pOther = other_stack_list.Head.pNext;
-		while (pOther)
+		Node2* pThis = &Head;
+		do
 		{
-			push(pOther->data);
+			Node2* newCur=new Node2(pOther->data);
 			pOther = pOther->pNext;
-		}
+			pThis->pNext = newCur;
+			pThis = newCur;
+		} while (pOther);
+		m_size = other_stack_list.m_size;
 	}
 }
 
@@ -105,14 +109,14 @@ inline MyStack2<T>& MyStack2<T>::operator=(const MyStack2<T>& source_stack_list)
 		Node2* pOther = source_stack_list.Head.pNext;
 		while (pThis)
 		{
-			pThis->p_data = pOther->p_data;
+			pThis->data = pOther->data;
 			pOther = pOther->pNext;
 			pThis = pThis->pNext;
 		}
 		//стек приемник меньше копируемого, добавляем в приемник оставшиеся узлы
 		while (pOther)
 		{
-			push(pOther.data);
+			push(pOther->data);
 			pOther = pOther->pNext;
 		}
 	}
@@ -124,12 +128,15 @@ inline MyStack2<T>& MyStack2<T>::operator=(MyStack2<T>&& temp_src_list_stack)
 {
 	if (this != &temp_src_list_stack)
 	{
-		while (Head.pNext)	pop();//очищаем список приемник
-		Head.pNext = temp_src_list_stack.pNext;
+		while (Head.pNext)	
+		{
+			pop();//очищаем список приемник
+		}
+		Head.pNext = temp_src_list_stack.Head.pNext;
 		m_size = temp_src_list_stack.m_size;
-		delete temp_src_list_stack;
 		temp_src_list_stack.m_size = 0;
 		temp_src_list_stack.Head.pNext = nullptr;
+		temp_src_list_stack.~MyStack2();
 	}
 	return *this;
 }
@@ -150,46 +157,52 @@ inline MyStack2<T>::~MyStack2()
 template<typename T>
 inline void MyStack2<T>::push(const T& e)
 {
-	Node2* tmp_node = new Node2;
-	tmp_node->pNext = &Head;
-	Head = *tmp_node;
-	Head.pNext->data= e;
+	Node2* tmp_node = new Node2(e);
+	tmp_node->pNext = Head.pNext;
+	Head.pNext = tmp_node;
+	//Head.pNext->data= e;
 	m_size++;
 }
 
 template<typename T>
 inline void MyStack2<T>::push(T&& e)
 {
-	Node2* tmp_node = new Node2;
-	tmp_node->pNext = &Head;
-	Head = *tmp_node;
-	Head.pNext->data = std::move(e);
+	Node2* tmp_node = new Node2(e);
+	tmp_node->pNext = Head.pNext;
+	Head.pNext = tmp_node;
+	//Head.pNext->data = std::move(e);
 	m_size++;
 }
 
 
 template<typename T>
-inline T& MyStack2<T>::pop()
+inline T MyStack2<T>::pop()
 {
 	if (m_size)
 	{
-		T res = std::move(Head.pNext->data);
+		T res = Head.pNext->data;
 		m_size--;
-		Node2* node_to_delete = &Head;
-		Head=Head.pNext;
+		Node2* node_to_delete = Head.pNext;
+		Head.pNext=Head.pNext->pNext;
 		delete node_to_delete;
 		return res;
 	}
 	else throw std::out_of_range("pop from epty stack!");
 }
 
- /*template<typename T>MyStack2<T>::
+template<typename T>
 ostream& operator<<(ostream& out, const MyStack2<T>& stack_to_print)
 {
-	MyStack2<T>::Node2* pCur = stack_to_print.pNext;
-	while (pCur)
-	{
-		out << pCur->data << " ";
-		pCur = pCur->pNext;
-	}
-}*/
+	return out << *stack_to_print.Head.pNext;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& stream,  const typename MyStack2<T>::Node2& node_to_print)
+{
+	
+		if (!node_to_print.pNext)
+			return stream << node_to_print.p_data << " ";
+		else
+			stream << node_to_print.pNext;
+	
+}
