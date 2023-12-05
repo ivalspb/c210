e616d09c5e769e08545d800ddb799054a00a30e5
ivalspb <ival.spb@gmail.com>
2023-12-05 18:47:08 +0300

    lab3 vect double +

---
+++ lab3-c210/my_vector.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using namespace std;
+
+template <typename T> void printVector(vector<T> v)
+{
+	cout << endl << "Vector have:" <<  " ";
+	cout << "size = " << v.size()<< " ";
+	cout << "capacity = " << v.capacity() <<  " ";
+	cout << "max_size = " << v.max_size() << endl;
+	for (size_t i = 0; i < v.size(); i++) cout << v[i] << " ";
+}
