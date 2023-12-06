b27a94a492ac1399430a9cff19c77a76e6be60b8
ALEIVAN@live.spbstu.ru <ALEIVAN@live.spbstu.ru>
2023-12-06 16:24:17 +0300

    lab3 vector insert?

--- lab3-c210/my_vector.h
+++ lab3-c210/my_vector.h
@@ -5,8 +5,25 @@ using namespace std;
 template <typename T> void printVector(vector<T>& v)
 {
 	cout << endl << "Vector have:" <<  " ";
-	cout << "size = " << v.size()<< " ";
-	cout << "capacity = " << v.capacity() <<  " ";
+	cout << "size = " << v.size()<< "\t";
+	cout << "capacity = " << v.capacity() <<  "\t";
 	cout << "max_size = " << v.max_size() << endl;
 	for (size_t i = 0; i < v.size(); i++) cout << v[i] << " ";
+}
+
+template <typename T> void printVector(vector<T*>& v)
+{
+	cout << endl << "pVector have:" << " ";
+	cout << "size = " << v.size() << "\t";
+	cout << "capacity = " << v.capacity() << "\t";
+	cout << "max_size = " << v.max_size() << endl;
+	for (size_t i = 0; i < v.size(); i++) cout << *v[i] << " ";
+}
+
+template<typename T> void unic_push(vector<T>& v, T& t)
+{
+	typename vector<T>::iterator it = v.begin(), it_end=v.end();
+	for (it; it != it_end; ++it)
+		if (*it == t) return;
+	v.insert(v.begin,t);
 }
